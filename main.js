56bc71bcfdb1ddd5f3e08508cd9efe3d613043f5
Jeremy Birnbaum <thejoo44@yahoo.com>
2020-02-04 21:52:09 -0700

    Adds turn indicator

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 /*----- constants -----*/
-
+// Defines the board and player colors
 var colors = {
   null: 'var(--null)',
-  playerX: 'var(--player1)',
-  playerO: 'var:(--playerO)',
+  player1: 'var(--player1)',
+  player0: 'var:(--playerO)',
 };
 
+// Winning combinations
 var winCombo = [
   [0, 1, 2],
   [3, 4, 5],
@@ -22,26 +23,30 @@ var winCombo = [
 let board = [];
 let playerTurn;
 let winDrawPlay;
+// how do I link the colors to this? Freezes browser if not 1 & -1
 let playerX = 1;
 let playerO = -1;
 
 /*----- cached element references -----*/
 var squaresEls = document.querySelectorAll('.squares');
-var message = document.querySelectorAll('h1');
+var message = document.querySelector('h1');
 let gameEl = document.querySelector('.game-board');
 
 /*----- event listeners -----*/
+// selects the squares when clicked(runs turn function)
 gameEl.addEventListener('click', turn) 
 
+// replay button
 document.querySelector('button').addEventListener('click', init)
 
 
 /*----- functions -----*/
 init()
 
+
 function init() {
   board = Array(9).fill(null)
-  console.log(board);
+  // console.log(board);
   gameBoard();
   playerTurn = playerX;
   winDrawPlay = null;
@@ -49,30 +54,42 @@ function init() {
 }
 
 function getWinner() {
+  // loops through the winCombo array to check for  
   for (let i = 0; i < winCombo; i++) {
-    if (Math.abs(board[winCombo[i][0]] + board[winCombo[i][1]] + board[winCombo[i][2]]) === 3){
+    // checks board for winning combos looping through the array.  Why does it equal 3?
+    if (Math.abs(board[winCombo[i][0]] + board[winCombo[i][1]] + board[winCombo[i][2]]) === 3) {
       return board[winCombo[i][0]]
     } 
   } 
+  // Outside of the winCombo loop, if the board contains any null values, the game continues
   if (board.includes(null)) {
     return null
   } 
+  // if all squares are taken and no winner, the game is tied
     return 'T'
 }
 
+// 
 function gameBoard() {
   for (i = 0; i < squaresEls.length; i++) {
     squaresEls[i] = board[i];
   }
-  console.log(board)
+  // console.log(board)
 }
 
 function turn(e) {
   e.target.innerText = playerTurn;
+  // doesn't end when there is a winner & allows boxes to be clicked multiple times
+  if (playerTurn === playerX) {
+    playerTurn = playerO
+  } else {
+    playerTurn = playerX
+  }
+  console.log('playerTurn', playerTurn)
   if (board[e.target.innerText] !== null) return;
   if (winDrawPlay !== null) return;
   winDrawPlay = getWinner();
-  playerTurn *= -1;
+  // playerTurn *= -1;
   render();
 }
 
@@ -81,9 +98,9 @@ function render() {
     squaresEls[idx].style.background = colors[sq]
   });
   if (winDrawPlay === null) {
-    message.innerHTML = (`It is ${playerTurn}/'s turn`)
+    message.innerHTML = (`It is ${playerTurn}'s turn`)
   } else if (winDrawPlay === 'T') {
-    message.innerHTML = (`It/'s A Tie`)
+    message.innerHTML = (`It's A Tie`)
   } else {
     message.innerHTML = (`Congrats ${playerTurn}! You Win!!`)
   }
